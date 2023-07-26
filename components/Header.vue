dd05c7c5a1c4e73dffe905d19548d5853ced88ab
yuto14prog <yuto14prog@gmail.com>
2023-07-26 12:41:57 +0900

    WIP：Headerをマークアップ

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="flex flex-nowrap flex-row justify-between pt-6">
+    <div>
+      <h1 v-text="$dayjs().format('MM/DD dddd')" class="text-3xl" />
+    </div>
+    <nav>
+      <ul class="text-xl flex">
+        <li class="px-1"><a class="block rounded-md px-2 py-1 hover:bg-theme-green" href="">Home</a></li>
+        <li class="px-1"><a class="block rounded-md px-2 py-1 hover:bg-theme-green" href="">Headline</a></li>
+        <li class="px-1"><a class="block rounded-md px-2 py-1 hover:bg-theme-green" href="">.......</a></li>
+        <li class="px-1"><a class="block rounded-md px-2 py-1 hover:bg-theme-green" href="">,,,,,,,</a></li>
+      </ul>
+    </nav>
+    <div>
+      <h1 class="text-red-700">ブックマーク</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
