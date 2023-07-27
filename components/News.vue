94132e96a29a00434ce8176cc28ad5dc4c72a32c
yuto14prog <yuto14prog@gmail.com>
2023-07-27 12:45:18 +0900

    api呼び出しまで

---
+++ components/News.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="max-w-[1055px] pt-8">
+    <h2 class="text-3xl text-center">Pick up</h2>
+  </div>
+</template>
+
+<script setup>
+  const config = useRuntimeConfig();
+  const {data: news} = await useFetch(
+    'https://newsapi.org/v2/top-headlines',
+    {query: {country: 'jp', apiKey: config.public.newskey}},
+  )
+</script>
