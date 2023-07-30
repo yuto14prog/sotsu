5d1a5935b3649ecc7c6f794bd91cdb317abb9193
yuto14prog <yuto14prog@gmail.com>
2023-07-30 12:02:39 +0900

    PC切り替えるのでWIPコミット

---
+++ components/Article.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="max-w-[520px] h-[135px] border-2 rounded-xl bg-white">
+    <h3>{{title}}</h3>
+    <p>{{publishedAt}}</p>
+    <img src="">
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    title: String,
+    publishedAt: String,
+    url: String,
+    urlToImage: String,
+  })
+</script>
--- components/News.vue
+++ components/News.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="max-w-[1055px] pt-8">
     <h2 class="text-3xl text-center">Pick up</h2>
+    <Article :title=wip.title :published-at=wip.publishedAt :url-to-image=wip.urlToImage />
   </div>
 </template>
 
@@ -10,4 +11,7 @@
     'https://newsapi.org/v2/top-headlines',
     {query: {country: 'jp', apiKey: config.public.newskey}},
   )
+  const articles = news.value.articles
+  const wip = articles[0]
+  console.log(wip);
 </script>
