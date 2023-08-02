66a5d2ccd73ccfe83845ae34165932285ebb414a
yuto14prog <yuto14prog@gmail.com>
2023-08-02 11:27:39 +0900

    今日の天気まで

--- components/Weather.vue
+++ components/Weather.vue
@@ -1,3 +1,35 @@
 <template>
-  
+  <div class="w-[520px] border-2 rounded-xl bg-white py-2 px-4">
+    <h2 class="text-3xl underline decoration-theme-green">Osaka</h2>
+    <div class="flex justify-between pt-2">
+      <div>
+        <p class="text-gray-500">気温</p>
+        <p><span class="text-4xl">{{Math.floor(parseInt(current.temp) - 273.16)}}</span>℃</p>
+      </div>
+      <div>
+        <p class="text-gray-500">体感温度</p>
+        <p><span class="text-4xl">{{Math.floor(parseInt(current.feels_like) - 273.16)}}</span>℃</p>
+      </div>
+      <div>
+        <p class="text-gray-500">湿度</p>
+        <p><span class="text-4xl">{{parseInt(current.humidity)}}</span>%</p>
+      </div>
+      <img :src="`https://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`" class="w-[70px] object-none bg-theme-green">
+    </div>
+  </div>
 </template>
+
+<script setup>
+  const config = useRuntimeConfig();
+  const lat = 34.6937
+  const lon = 135.5021
+  const exclude = 'minutely'
+  const {data: weather} = await useFetch(
+    'https://api.openweathermap.org/data/2.5/onecall',
+    {query: {lat: lat, lon: lon, units: 'metrics', exclude: exclude, appid: config.public.weatherkey}}
+  )
+  const current = await weather.value.current
+  const daily = await weather.value.daily
+  console.log(daily[0].temp.max);
+</script>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
     newskey: '',
+    weatherkey: '',
     }
   },
 })
